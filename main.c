154577d3d9cba87f988e96b626f94a0b97b19cc4
Marco Vomiero <colocomarco1@gmail.com>
2022-12-16 18:41:28 +0100

    main change flags

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 12:16:03 by mvomiero          #+#    #+#             */
-/*   Updated: 2022/12/16 13:13:53 by mvomiero         ###   ########.fr       */
+/*   Updated: 2022/12/16 17:10:13 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 int main(void)
 {
-	void *ptr = 0;
+/* 	void *ptr = 0;
 
     printf("%p\n", ptr);
     printf("%u\n", UINT_MAX);
@@ -26,5 +26,16 @@ int main(void)
     printf("%x\n", UINT_MAX);
 
 	printf("ciao %s oggi e il n %d\n", "Marco", 16);
-	ft_printf("ciao %s oggi e il n %d\n", "Marco", 16);
+	ft_printf("ciao %s oggi e il n %d\n", "Marco", 16); */
+
+	printf( "%12d\n", 452 );
+	printf( "%9d\n", 452 );
+	printf( "%+9d\n", 452 );
+	printf( "%+d\n", 452 );
+	printf( "%+09d\n", 452 );
+    printf( "%09d\n", 452 );
+    printf( "% 9d\n", 452 );
+	printf("Prova %0d, zero\n", 12);
+	printf("Prova %.21d, precision\n", 23);
+	printf("Prova%        d, it is a space, just adds a space before positive numbers\n", 23);
 }
